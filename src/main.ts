46b9a49cf14d64c843b0cb5c86421c8dcd144da1
sales <87090979+SallesTachiyomi@users.noreply.github.com>
2024-10-28 10:21:23 -0400

    asa

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,30 @@ app.get("/produtos", async(req,res)=>{
     }
 })
 
+app.post("/produtos", async(req,res)=>{
+    //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
+    //1 - Criar a conexão com o banco
+    try{
+        const conection = await mysql.createConnection({
+            host:process.env.dbhost?process.env.dbhost:"localhost",
+            user:process.env.dbuser?process.env.dbuser:"root",
+            password:process.env.dbpassword?process.env.dbpassword:"",
+            database:process.env.dbname?process.env.dbname:"banco1022a",
+            port:process.env.dbport?parseInt(process.env.dbport):3306
+        })
+        //2 - Realizar uma consulta na tabela
+        const {id,nome,descricao,preco,imagem} = req.body
+        const [result, fields] = await conection.query("INSERT INTO produtos VALUES (?,?,?,?,?)",
+            [id,nome,descricao,preco,imagem])
+        await conection.end()
+        //3 - Devolver os dados pra quem pediu
+        res.send(result)
+    }catch(e){
+        console.log(e)
+        res.status(500).send("Server ERROR")
+    }
+})
+
 app.get("/usuarios", async(req,res)=>{
     //OK -> 0 - Criar o banco de dados e iniciar o servidor de banco.
     //1 - Criar a conexão com o banco
